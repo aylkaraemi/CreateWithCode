e2e91e66b2a6325fdf7d391bc4d7a7f9decfacbb
Kayla Carpenter <aylkaraemi@gmail.com>
2019-10-25 21:05:41 -0600

    Add follow player functionality to camera

---
+++ Prototype1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // repositions camera based on player position, offset above & behind
+        transform.position = player.transform.position + offset;
+    }
+}
