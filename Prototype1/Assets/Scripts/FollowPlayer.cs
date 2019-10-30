d954683e4d2e76cf14ce6d637ace2cd173cc9ed6
Kayla Carpenter <aylkaraemi@gmail.com>
2019-10-29 21:31:52 -0600

    Add player control functionality to vehicle script

--- Prototype1/Assets/Scripts/FollowPlayer.cs
+++ Prototype1/Assets/Scripts/FollowPlayer.cs
@@ -5,13 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -7);
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector3 offset = new Vector3(0, 5, -7); // sets position relative to player object
 
     // Update is called once per frame
     void Update()
--- Prototype1/Assets/Scripts/PlayerController.cs
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20.0f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float speed = 20.0f;
+    private float turnSpeed = 45;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed); // increase z by 1 * the time passed * 20 = 20m/sec
+        // Player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Move the vehicle forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates car based on horizontal input
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
