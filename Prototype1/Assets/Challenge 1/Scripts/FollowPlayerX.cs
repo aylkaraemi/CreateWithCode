7f5f9167f06d3b23c70fa7ceb359ed17bb7aa777
Kayla Carpenter <aylkaraemi@gmail.com>
2019-10-30 15:55:31 -0600

    Challenge1: Set camera position based on plane position

--- Prototype1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ Prototype1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    private Vector3 offset = (30, 0, 5);
 
     // Start is called before the first frame update
     void Start()
